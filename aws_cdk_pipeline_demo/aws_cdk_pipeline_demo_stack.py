5a9679418c1ff15cca5d2115bcf2a5c57fef3a5e
EC2 Default User <ec2-user@ip-172-31-68-215.ec2.internal>
2024-02-19 11:41:34 +0000

    repo modified

--- aws_cdk_pipeline_demo/aws_cdk_pipeline_demo_stack.py
+++ aws_cdk_pipeline_demo/aws_cdk_pipeline_demo_stack.py
@@ -13,7 +13,7 @@ class AwsCdkPipelineDemoStack(Stack):
         pipeline =  pipelines.CodePipeline(self, "Pipeline",
                         pipeline_name="MyPipeline",
                         synth=pipelines.ShellStep("Synth",
-                            input=pipelines.CodePipelineSource.git_hub("OWNER/REPO", "main"),
+                            input=pipelines.CodePipelineSource.git_hub("ayushpandey09/aws_cdk_pipeline_demo.git", "main"),
                             commands=["npm install -g aws-cdk",
                                 "python -m pip install -r requirements.txt",
                                 "cdk synth"]
